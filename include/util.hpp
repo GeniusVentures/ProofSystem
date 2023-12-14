5dceb1b04028ae5ea93b17dd60d7d9bd4bfdb9eb
Henrique A Klein <henryaklein@gmail.com>
2023-12-14 17:11:37 -0300

    Feat: Adding utilities functions

--- include/util.hpp
+++ include/util.hpp
@@ -10,16 +10,54 @@
 #include <iomanip>
 #include <vector>
 
-namespace util {
+namespace util
+{
 
     /**
      * Convert a byte array to a hexadecimal string.
      * @param bytes A vector of bytes to be converted.
      * @return A hexadecimal string representation of the bytes.
      */
-    std::string to_string(const std::vector<unsigned char>& bytes);
+    std::string to_string( const std::vector<unsigned char> &bytes );
 
     // Additional utility functions can be declared here.
+
+    static bool isLittleEndian()
+    {
+        std::uint32_t num     = 1;
+        std::uint8_t *bytePtr = reinterpret_cast<std::uint8_t *>( &num );
+
+        return ( *bytePtr == 1 );
+    }
+
+    static std::int32_t HexASCII2Num( char *p_char, std::size_t num_nibbles_resolution )
+    {
+        std::int32_t sum = 0;
+
+        for ( std::int32_t i = num_nibbles_resolution - 1; i >= 0; --i )
+        {
+            if ( std::isdigit( p_char[i] ) )
+            {
+                sum += ( *p_char - '0' ) << ( 8 * i );
+            }
+            else
+            {
+                sum += ( std::toupper( *p_char ) - 'A' + 10 ) << ( 8 * i );
+            }
+        }
+
+        return sum;
+    }
+    static std::vector<std::uint32_t> HexASCII2Num( char *p_char, std::size_t char_ptr_size, std::size_t num_nibbles_resolution )
+    {
+        std::vector<std::uint32_t> out_vect;
+
+        for ( std::size_t i = 0; i < char_ptr_size; i += num_nibbles_resolution )
+        {
+            out_vect.push_back( HexASCII2Num( &p_char[i], num_nibbles_resolution ) );
+        }
+        return out_vect;
+    }
 
 }
 
