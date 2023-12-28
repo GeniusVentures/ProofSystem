d6bc1b667a01e8123a16336fafd70ebf1513d721
Henrique A Klein <henryaklein@gmail.com>
2023-12-28 12:01:45 -0300

    Feat: Adding zero from random scalar

--- include/PallasMapper.hpp
+++ include/PallasMapper.hpp
@@ -8,13 +8,40 @@ class PallasMapper
     using CurveType      = nil::crypto3::algebra::curves::pallas;
     using generator_type = nil::crypto3::random::algebraic_random_device<CurveType::scalar_field_type>;
 
+    
 private:
     /* data */
-    static generator_type random_gen;
+    curves::pallas::g1_type<>::value_type zero_g1_value;
+    curves::pallas::g1_type<>::value_type curr_value;
+
 
 public:
     PallasMapper( /* args */ );
+    PallasMapper(curves::pallas::scalar_field_type::value_type random_val);
     ~PallasMapper();
+    static generator_type random_gen;
+    PallasMapper& operator+=(const std::int32_t value)
+    {
+        auto val_multi = static_cast<nil::crypto3::multiprecision::number<nil::crypto3::multiprecision::cpp_int_backend<>>>(value);
+        curves::pallas::g1_type<>::value_type g1_value = val_multi*curves::pallas::g1_type<>::value_type().one();
+        curr_value+=g1_value;
+        return *this;
+    }
+    PallasMapper& operator-=(const std::int32_t value)
+    {
+        auto val_multi = static_cast<nil::crypto3::multiprecision::number<nil::crypto3::multiprecision::cpp_int_backend<>>>(value);
+        curves::pallas::g1_type<>::value_type g1_value = val_multi*curves::pallas::g1_type<>::value_type().one();
+        curr_value-=g1_value;
+        return *this;
+    }
+
+    bool operator==(const std::int32_t value)
+    {
+        auto val_multi = static_cast<nil::crypto3::multiprecision::number<nil::crypto3::multiprecision::cpp_int_backend<>>>(value);
+        curves::pallas::g1_type<>::value_type g1_value = val_multi*curves::pallas::g1_type<>::value_type().one();
+
+        return curr_value==g1_value; 
+    }
 };
 
 PallasMapper::generator_type PallasMapper::random_gen;
@@ -22,19 +49,14 @@ PallasMapper::generator_type PallasMapper::random_gen;
 PallasMapper::PallasMapper( /* args */ )
 {
     curves::pallas::scalar_field_type::value_type random_scalar_value = random_gen();
+    zero_g1_value = random_scalar_value*curves::pallas::g1_type<>::value_type().one() - random_scalar_value*curves::pallas::g1_type<>::value_type().one();
+    curr_value = zero_g1_value;
 
-    //curves::pallas::g1_type<>::value_type random_g1_value = random_element<typename CurveType::template g1_type<>>();
-    curves::pallas::g1_type<>::value_type random_g1_value = random_scalar_value*curves::pallas::g1_type<>::value_type().one();
-    curves::pallas::g1_type<>::value_type zero_g1_value;
-
-
-    std::cout << "zero_g1_value.to_affine().X.data:  " << zero_g1_value.to_affine().X.data << std::endl;
-    std::cout << "zero_g1_value.X.data " << zero_g1_value.X.data << std::endl;
-    std::cout << "zero_g1_value.is_well_formed() " << zero_g1_value.is_well_formed() << std::endl;
-    std::cout << "random_g1_value.to_affine().X.data " << random_g1_value.to_affine().X.data << std::endl;
-    std::cout << "random_g1_value.to_affine().X.data " << random_g1_value.X.data << std::endl;
-    std::cout << "random_g1_value.is_well_formed() " << random_g1_value.is_well_formed() << std::endl;
-
+}
+PallasMapper::PallasMapper(curves::pallas::scalar_field_type::value_type random_val )
+{
+    zero_g1_value = random_val*curves::pallas::g1_type<>::value_type().one() - random_val*curves::pallas::g1_type<>::value_type().one();
+    curr_value = zero_g1_value;
 }
 
 PallasMapper::~PallasMapper()
--- test/PallasMapper_test.cpp
+++ test/PallasMapper_test.cpp
@@ -4,4 +4,12 @@
 TEST( PallasMapperTest, PallasZeroMap )
 {
     PallasMapper mapper;
+
+    EXPECT_TRUE(mapper==0);
+    mapper+=10;
+    mapper+=10;
+    EXPECT_TRUE(mapper==20);
+    EXPECT_FALSE(mapper==30);
+    mapper-=10;
+    EXPECT_TRUE(mapper==10);
 }
