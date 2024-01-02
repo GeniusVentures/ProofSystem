c74c0461734ad161ff31b0abacc71a8e050b4268
Henrique A Klein <henryaklein@gmail.com>
2024-01-02 13:14:33 -0300

    Feat: Encoding the signed data

--- include/KDFGenerator.hpp
+++ include/KDFGenerator.hpp
@@ -1,37 +1,63 @@
+#include <vector>
 #include <nil/crypto3/hash/algorithm/hash.hpp>
 #include <nil/crypto3/hash/sha2.hpp>
 #include <nil/crypto3/pubkey/algorithm/sign.hpp>
 #include <nil/crypto3/pubkey/algorithm/verify.hpp>
-#include <nil/crypto3/pubkey/algorithm/encrypt.hpp>
+#include <nil/crypto3/block/algorithm/encrypt.hpp>
+#include <nil/crypto3/block/algorithm/decrypt.hpp>
+#include <nil/crypto3/algebra/marshalling.hpp>
+#include <nil/crypto3/block/aes.hpp>
+#include <nil/crypto3/block/rijndael.hpp>
 #include "BitcoinKeyGenerator.hpp"
 
 class KDFGenerator
 {
 public:
-    using SignatureType = typename pubkey::public_key<bitcoin::policy_type>::signature_type ;
+    using SignatureType = typename pubkey::public_key<bitcoin::policy_type>::signature_type;
     KDFGenerator();
-    KDFGenerator( const pubkey::public_key<bitcoin::policy_type>::signature_type &imported_secret, const std::string &senders_key,
-                  const std::string &pub_key_own_value );
 
-    static SignatureType GenerateSharedSecret( const pubkey::private_key<bitcoin::policy_type> &prvt_key, const std::string &pub_key_value );
-    static bool          CheckSharedSecret( const SignatureType &signed_msg, const pubkey::private_key<bitcoin::policy_type> &prvt_key,
+    static std::string GenerateSharedSecret( const pubkey::private_key<bitcoin::policy_type> &prvt_key, const std::string &pub_key_value );
+    static bool          CheckSharedSecret( const std::string &signed_msg, const pubkey::private_key<bitcoin::policy_type> &prvt_key,
                                             const std::string &value );
     ~KDFGenerator();
+
 private:
     SignatureType secret_sign;
-
 };
-KDFGenerator::SignatureType KDFGenerator::GenerateSharedSecret( const pubkey::private_key<bitcoin::policy_type> &prvt_key,
-                                                                const std::string                                &pub_key_value )
+std::string KDFGenerator::GenerateSharedSecret( const pubkey::private_key<bitcoin::policy_type> &prvt_key, const std::string &pub_key_value )
 {
-    return sign<bitcoin::policy_type>( pub_key_value, prvt_key );
+    KDFGenerator::SignatureType signed_secret = sign<bitcoin::policy_type>( pub_key_value, prvt_key );
+    std::vector<std::uint8_t>   signed_vector( 64 );
+
+    nil::marshalling::bincode::field<bitcoin::scalar_field_type>::field_element_to_bytes<std::vector<std::uint8_t>::iterator>(
+        std::get<0>( signed_secret ), signed_vector.begin(), signed_vector.begin() + signed_vector.size() / 2 );
+    nil::marshalling::bincode::field<bitcoin::scalar_field_type>::field_element_to_bytes<std::vector<std::uint8_t>::iterator>(
+        std::get<1>( signed_secret ), signed_vector.begin() + signed_vector.size() / 2, signed_vector.end() );
+
+    std::vector<std::uint8_t> key_vector = util::HexASCII2NumStr<std::uint8_t>( pub_key_value.data(), pub_key_value.size() );
+
+    std::string out = nil::crypto3::encrypt<nil::crypto3::block::aes<256>>( signed_vector, key_vector );
+    return out;
 }
-bool KDFGenerator::CheckSharedSecret( const SignatureType &signed_msg, const pubkey::private_key<bitcoin::policy_type> &prvt_key,
+bool KDFGenerator::CheckSharedSecret( const std::string &signed_msg, const pubkey::private_key<bitcoin::policy_type> &prvt_key,
                                       const std::string &value )
 {
-    const auto &pubkey = static_cast<pubkey::public_key<bitcoin::policy_type>>( prvt_key );
+    std::vector<std::uint8_t> key_vector = util::HexASCII2NumStr<std::uint8_t>( value.data(), value.size() );
+    const auto               &pubkey     = static_cast<pubkey::public_key<bitcoin::policy_type>>( prvt_key );
 
-    return static_cast<bool>( verify<bitcoin::policy_type>( value, signed_msg, pubkey ) );
+    std::vector<std::uint8_t> signed_vector = util::HexASCII2NumStr<std::uint8_t>( signed_msg.data(), signed_msg.size() );
+    std::reverse(signed_vector.begin(),signed_vector.end());
+
+    std::cout << std::endl;
+    std::vector<std::uint8_t> decrypted_string = nil::crypto3::decrypt<nil::crypto3::block::aes<256>>( signed_vector, key_vector );
+
+    auto my_first_value = nil::marshalling::bincode::field<bitcoin::scalar_field_type>::field_element_from_bytes<std::vector<std::uint8_t>::iterator>(
+        decrypted_string.begin(), decrypted_string.begin() + decrypted_string.size() / 2 );
+    auto my_second_value = nil::marshalling::bincode::field<bitcoin::scalar_field_type>::field_element_from_bytes<std::vector<std::uint8_t>::iterator>(
+        decrypted_string.begin() + decrypted_string.size() / 2, decrypted_string.end() );
+
+    SignatureType signed_data(my_first_value.second,my_second_value.second);
+    return static_cast<bool>( verify<bitcoin::policy_type>( value, signed_data, pubkey ) );
 }
 KDFGenerator::KDFGenerator()
 {
--- test/KDFGenerator_test.cpp
+++ test/KDFGenerator_test.cpp
@@ -7,10 +7,12 @@ TEST( KDFGeneratorTest, KDFGeneratorMaster )
 {
     BitcoinKeyGenerator bitcoin_keygen;
 
-    std::string test_message = "JUST A MESSAGE";
+    std::string test_key = "031e7bcc70c72770dbb72fea022e8a6d07f814d2ebe4de9ae3f7af75bf706902a7";
 
-    auto signed_key = KDFGenerator::GenerateSharedSecret(bitcoin_keygen.get_private_key(),test_message);
+    auto signed_key = KDFGenerator::GenerateSharedSecret( bitcoin_keygen.get_private_key(), test_key );
 
-    EXPECT_TRUE(KDFGenerator::CheckSharedSecret(signed_key,bitcoin_keygen.get_private_key(), test_message));
-    EXPECT_FALSE(KDFGenerator::CheckSharedSecret(signed_key,bitcoin_keygen.get_private_key(), "WRONG MESSAGE"));
+    EXPECT_EQ( signed_key.size(), 128 );
+    EXPECT_TRUE( KDFGenerator::CheckSharedSecret( signed_key, bitcoin_keygen.get_private_key(), test_key ) );
+    EXPECT_FALSE( KDFGenerator::CheckSharedSecret( signed_key, bitcoin_keygen.get_private_key(),
+                                                   "088e7bcc70c72770dbb72fea022e8a6d07f814d2ebe4de9ae3f7af75bf706902a7" ) );
 }
