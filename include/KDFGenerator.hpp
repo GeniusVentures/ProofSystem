9e1b3bbd54dc6a7e162e43da1a10a67b4bf3498b
Henrique A Klein <henryaklein@gmail.com>
2024-01-04 16:00:55 -0300

    Feat: Adding public key import

--- include/KDFGenerator.hpp
+++ include/KDFGenerator.hpp
@@ -8,61 +8,86 @@
 #include <nil/crypto3/algebra/marshalling.hpp>
 #include <nil/crypto3/block/aes.hpp>
 #include <nil/crypto3/block/rijndael.hpp>
-#include "BitcoinKeyGenerator.hpp"
+#include "ECDSATypes.hpp"
 
+template <typename PolicyType>
 class KDFGenerator
 {
 public:
-    using SignatureType = typename pubkey::public_key<bitcoin::policy_type>::signature_type;
-    KDFGenerator();
+    using SignatureType   = typename pubkey::public_key<PolicyType>::signature_type;
+    using SgnusPubKeyType = std::string;
+    using ECDSAPubKey     = std::string;
 
-    static std::string GenerateSharedSecret( const pubkey::private_key<bitcoin::policy_type> &prvt_key, const std::string &pub_key_value );
-    static bool          CheckSharedSecret( const std::string &signed_msg, const pubkey::private_key<bitcoin::policy_type> &prvt_key,
-                                            const std::string &value );
+    static std::string GenerateSharedSecret( const pubkey::private_key<PolicyType> &prvt_key, const SgnusPubKeyType &sgnus_key );
+    static bool        CheckSharedSecret( const std::string &signed_secret, const ECDSAPubKey &prover_key, const SgnusPubKeyType &sgnus_key );
+    KDFGenerator();
     ~KDFGenerator();
 
 private:
     SignatureType secret_sign;
+
+    static pubkey::public_key<PolicyType> BuildPublicKeyECDSA( const ECDSAPubKey &pubkey_data );
 };
-std::string KDFGenerator::GenerateSharedSecret( const pubkey::private_key<bitcoin::policy_type> &prvt_key, const std::string &pub_key_value )
+
+template <typename PolicyType>
+std::string KDFGenerator<PolicyType>::GenerateSharedSecret( const pubkey::private_key<PolicyType> &prvt_key, const SgnusPubKeyType &sgnus_key )
 {
-    KDFGenerator::SignatureType signed_secret = sign<bitcoin::policy_type>( pub_key_value, prvt_key );
+    KDFGenerator::SignatureType signed_secret = sign<PolicyType>( sgnus_key, prvt_key );
     std::vector<std::uint8_t>   signed_vector( 64 );
 
-    nil::marshalling::bincode::field<bitcoin::scalar_field_type>::field_element_to_bytes<std::vector<std::uint8_t>::iterator>(
+    nil::marshalling::bincode::field<ecdsa::scalar_field_type>::field_element_to_bytes<std::vector<std::uint8_t>::iterator>(
         std::get<0>( signed_secret ), signed_vector.begin(), signed_vector.begin() + signed_vector.size() / 2 );
-    nil::marshalling::bincode::field<bitcoin::scalar_field_type>::field_element_to_bytes<std::vector<std::uint8_t>::iterator>(
+    nil::marshalling::bincode::field<ecdsa::scalar_field_type>::field_element_to_bytes<std::vector<std::uint8_t>::iterator>(
         std::get<1>( signed_secret ), signed_vector.begin() + signed_vector.size() / 2, signed_vector.end() );
 
-    std::vector<std::uint8_t> key_vector = util::HexASCII2NumStr<std::uint8_t>( pub_key_value.data(), pub_key_value.size() );
+    std::vector<std::uint8_t> derived_key_vector =
+        static_cast<std::vector<std::uint8_t>>( hash<hashes::sha2<256>>( signed_vector.begin(), signed_vector.end() ) );
+    signed_vector.insert( signed_vector.end(), derived_key_vector.begin(), derived_key_vector.end() );
 
-    std::string out = nil::crypto3::encrypt<nil::crypto3::block::aes<256>>( signed_vector, key_vector );
-    return out;
+    std::vector<std::uint8_t> key_vector = util::HexASCII2NumStr<std::uint8_t>( sgnus_key.data(), sgnus_key.size() );
+
+    return static_cast<std::string>( nil::crypto3::encrypt<nil::crypto3::block::aes<256>>( signed_vector, key_vector ) );
 }
-bool KDFGenerator::CheckSharedSecret( const std::string &signed_msg, const pubkey::private_key<bitcoin::policy_type> &prvt_key,
-                                      const std::string &value )
+template <typename PolicyType>
+bool KDFGenerator<PolicyType>::CheckSharedSecret( const std::string &signed_secret, const ECDSAPubKey &prover_key, const SgnusPubKeyType &sgnus_key )
 {
-    std::vector<std::uint8_t> key_vector = util::HexASCII2NumStr<std::uint8_t>( value.data(), value.size() );
-    const auto               &pubkey     = static_cast<pubkey::public_key<bitcoin::policy_type>>( prvt_key );
+    std::vector<std::uint8_t> key_vector = util::HexASCII2NumStr<std::uint8_t>( sgnus_key.data(), sgnus_key.size() );
+    const auto                pubkey     = BuildPublicKeyECDSA( prover_key );
 
-    std::vector<std::uint8_t> signed_vector = util::HexASCII2NumStr<std::uint8_t>( signed_msg.data(), signed_msg.size() );
-    std::reverse(signed_vector.begin(),signed_vector.end());
+    std::vector<std::uint8_t> signed_vector = util::HexASCII2NumStr<std::uint8_t>( signed_secret.data(), signed_secret.size() );
+    std::reverse( signed_vector.begin(), signed_vector.end() );
 
-    std::cout << std::endl;
     std::vector<std::uint8_t> decrypted_string = nil::crypto3::decrypt<nil::crypto3::block::aes<256>>( signed_vector, key_vector );
 
-    auto my_first_value = nil::marshalling::bincode::field<bitcoin::scalar_field_type>::field_element_from_bytes<std::vector<std::uint8_t>::iterator>(
-        decrypted_string.begin(), decrypted_string.begin() + decrypted_string.size() / 2 );
-    auto my_second_value = nil::marshalling::bincode::field<bitcoin::scalar_field_type>::field_element_from_bytes<std::vector<std::uint8_t>::iterator>(
-        decrypted_string.begin() + decrypted_string.size() / 2, decrypted_string.end() );
+    auto my_first_value = nil::marshalling::bincode::field<ecdsa::scalar_field_type>::field_element_from_bytes<std::vector<std::uint8_t>::iterator>(
+        decrypted_string.begin(), decrypted_string.begin() + 32 );
+    auto my_second_value = nil::marshalling::bincode::field<ecdsa::scalar_field_type>::field_element_from_bytes<std::vector<std::uint8_t>::iterator>(
+        decrypted_string.begin() + 32, decrypted_string.begin() + 64 );
 
-    SignatureType signed_data(my_first_value.second,my_second_value.second);
-    return static_cast<bool>( verify<bitcoin::policy_type>( value, signed_data, pubkey ) );
+    SignatureType signed_data( my_first_value.second, my_second_value.second );
+
+    return static_cast<bool>( verify<PolicyType>( sgnus_key, signed_data, pubkey ) );
 }
-KDFGenerator::KDFGenerator()
+
+template <typename PolicyType>
+pubkey::public_key<PolicyType> KDFGenerator<PolicyType>::BuildPublicKeyECDSA( const ECDSAPubKey &pubkey_data )
+{
+    auto z_data_one = pubkey::public_key<PolicyType>::g1_value_type::field_type::value_type::one();
+
+    std::vector<std::uint8_t> key_vector = util::HexASCII2NumStr<std::uint8_t>( pubkey_data.data(), pubkey_data.size() );
+
+    auto y_data = nil::marshalling::bincode::field<ecdsa::base_field_type>::field_element_from_bytes<std::vector<std::uint8_t>::iterator>(
+        key_vector.begin(), key_vector.begin() + key_vector.size() / 2 );
+    auto x_data = nil::marshalling::bincode::field<ecdsa::base_field_type>::field_element_from_bytes<std::vector<std::uint8_t>::iterator>(
+        key_vector.begin() + key_vector.size() / 2, key_vector.end() );
+
+    return typename pubkey::public_key<PolicyType>::public_key_type( x_data.second, y_data.second, z_data_one );
+}
+template <typename PolicyType>
+KDFGenerator<PolicyType>::KDFGenerator()
 {
 }
-
-KDFGenerator::~KDFGenerator()
+template <typename PolicyType>
+KDFGenerator<PolicyType>::~KDFGenerator()
 {
 }
--- test/KDFGenerator_test.cpp
+++ test/KDFGenerator_test.cpp
@@ -7,12 +7,12 @@ TEST( KDFGeneratorTest, KDFGeneratorMaster )
 {
     BitcoinKeyGenerator bitcoin_keygen;
 
-    std::string test_key = "031e7bcc70c72770dbb72fea022e8a6d07f814d2ebe4de9ae3f7af75bf706902a7";
+    std::string test_sgnus_key = "031e7bcc70c72770dbb72fea022e8a6d07f814d2ebe4de9ae3f7af75bf706902a7";
+    std::string wrong_sgnus_key = "02aabbcc70c72770dbb72fea022e8a6d07f814d2ebe4de9ae3f7af75bf706902a7";
 
-    auto signed_key = KDFGenerator::GenerateSharedSecret( bitcoin_keygen.get_private_key(), test_key );
+    auto signed_key = KDFGenerator<bitcoin::policy_type>::GenerateSharedSecret( bitcoin_keygen.get_private_key(), test_sgnus_key );
 
-    EXPECT_EQ( signed_key.size(), 128 );
-    EXPECT_TRUE( KDFGenerator::CheckSharedSecret( signed_key, bitcoin_keygen.get_private_key(), test_key ) );
-    EXPECT_FALSE( KDFGenerator::CheckSharedSecret( signed_key, bitcoin_keygen.get_private_key(),
-                                                   "088e7bcc70c72770dbb72fea022e8a6d07f814d2ebe4de9ae3f7af75bf706902a7" ) );
+    EXPECT_EQ( signed_key.size(), 192 );
+    EXPECT_TRUE( KDFGenerator<bitcoin::policy_type>::CheckSharedSecret( signed_key, bitcoin_keygen.GetPublicKeyEntireValue(), test_sgnus_key ) );
+    EXPECT_FALSE( KDFGenerator<bitcoin::policy_type>::CheckSharedSecret( signed_key, bitcoin_keygen.GetPublicKeyEntireValue(), wrong_sgnus_key ) );
 }
