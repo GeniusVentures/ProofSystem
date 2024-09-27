326b8b1f6fa7dab069449e947faaae046193b19f
Brent <brent@gnus.ai>
2024-09-27 14:56:22 -0600

    Hijack existing gtest code, for monolith compile checking.

--- test/BitcoinKeyGenerator_test.cpp
+++ test/BitcoinKeyGenerator_test.cpp
@@ -10,7 +10,58 @@
 #include <nil/crypto3/codec/adaptor/coded.hpp>
 #include <nil/crypto3/codec/base.hpp>
 
+#include <nil/crypto3/hash/ripemd.hpp>
+
 using namespace bitcoin;
+
+//*******************************************************//
+/*
+MONOLITH TESTING.
+*/
+//*******************************************************//
+
+#include <nil/crypto3/hash/monolith_31.hpp>    
+
+//TODO: new monolith code test
+using namespace nil::crypto3;
+
+namespace monolith
+{
+
+    // using CurveType               = ecdsa_t::CurveType;                        ///< Curve type used by Ethereum keys
+    // using base_field_type         = typename ecdsa_t::base_field_type;         ///< Ethereum base field type
+    // using scalar_field_type       = typename ecdsa_t::scalar_field_type;       ///< Ethereum scalar field type
+    // using scalar_field_value_type = typename ecdsa_t::scalar_field_value_type; ///< Ethereum value from the scalar field type
+    // using random_generator_type   = ecdsa_t::random_generator_type;            ///< Random generator type, to generate new Ethereum keys/addresses
+    // using hash_type               = ecdsa_t::hash_type;                        ///< The hash type used by Ethereum address derivation
+    // using generator_type          = ecdsa_t::generator_type;                   ///< The deterministic generator used by Ethereum key pair
+    // using padding_policy          = ecdsa_t::padding_policy;                   ///< Ethereum passing policy
+    // using policy_type             = ecdsa_t::policy_type;                      ///< Ethereum policy type
+    // using signature_type          = ecdsa_t::signature_type;                   ///< Random generator type, to generate new Bitcoin keys/addresses
+    using FieldType = nil::crypto3::algebra::fields::pallas_base_field;
+    using derivation_hash_type    = hashes::monolith_31_compressor<FieldType,31>;                         ///< Example Monolith usage.
+
+}
+
+TEST()  //BRENT: The goal here, for now, is just to compile...not worried about testing results.
+{
+        std::vector<std::uint8_t> work_vect( pub_key_vect );
+        work_vect.push_back( ( pub_key_vect.front() % 2 ) ? PARITY_ODD_ID : PARITY_EVEN_ID );
+
+        work_vect = static_cast<std::vector<std::uint8_t>>( hash<derivation_hash_type>( work_vect.rbegin(), work_vect.rend() ) );
+        work_vect = static_cast<std::vector<std::uint8_t>>( hash<hashes::ripemd160>( work_vect ) );
+
+        work_vect.insert( work_vect.begin(), MAIN_NETWORK_ID );
+
+        std::array<std::uint8_t, 32> checksum = hash<derivation_hash_type>( work_vect );
+        checksum                              = hash<derivation_hash_type>( checksum );    
+}
+
+//*******************************************************//
+/*
+END MONOLITH TESTING.
+*/
+//*******************************************************//
 
 TEST( BitcoinKeyGeneratorTest, PrivateKeyGenerated )
 {
