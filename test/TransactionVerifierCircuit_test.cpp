03534bc8bc540222addbd081968da8acb31f7f17
Henrique A Klein <henryaklein@gmail.com>
2024-10-03 09:59:57 -0300

    Fix: Interface of the circuit method. Broken test due to limitation of the circifier

--- test/TransactionVerifierCircuit_test.cpp
+++ test/TransactionVerifierCircuit_test.cpp
@@ -13,8 +13,10 @@ using namespace nil::crypto3::algebra::curves;
 TEST( TransactionVerifierCircuitTest, ValidateTransactionPass )
 {
     // Test parameters
-    typename pallas::scalar_field_type::value_type                         balance = 1000;
-    typename pallas::scalar_field_type::value_type                         amount  = 500;
+    uint64_t balance = 1000;
+    uint64_t amount  = 500;
+    //typename pallas::scalar_field_type::value_type                         balance = 1000;
+    //typename pallas::scalar_field_type::value_type                         amount  = 500;
     typename pallas::template g1_type<coordinates::affine>::value_type     generator( 1, 2 );
     typename pallas::scalar_field_type::value_type                         base_seed     = 12345; // Example seed for TOTP
     typename pallas::scalar_field_type::value_type                         provided_totp = 67890; // Example provided TOTP
@@ -25,15 +27,15 @@ TEST( TransactionVerifierCircuitTest, ValidateTransactionPass )
     auto amount_commitment               = generator * amount;
     auto expected_new_balance_commitment = generator * ( balance - amount );
 
-    EXPECT_TRUE( ValidateTransaction( balance, amount, balance_commitment, amount_commitment, expected_new_balance_commitment, generator, ranges,
-                                      base_seed, provided_totp ) );
+    EXPECT_TRUE( ValidateTransaction( balance, amount, balance, amount, balance_commitment, amount_commitment, expected_new_balance_commitment,
+                                      generator, ranges, base_seed, provided_totp ) );
 }
 
 TEST( TransactionVerifierCircuitTest, ValidateTransactionFailNegativeBalance )
 {
     // Test parameters
-    typename pallas::scalar_field_type::value_type                         balance = 500;
-    typename pallas::scalar_field_type::value_type                         amount  = 1000;
+    uint64_t                                                               balance = 500;
+    uint64_t                                                               amount  = 1000;
     typename pallas::template g1_type<coordinates::affine>::value_type     generator( 1, 2 );
     typename pallas::scalar_field_type::value_type                         base_seed     = 12345; // Example seed for TOTP
     typename pallas::scalar_field_type::value_type                         provided_totp = 67890; // Example provided TOTP
@@ -44,15 +46,15 @@ TEST( TransactionVerifierCircuitTest, ValidateTransactionFailNegativeBalance )
     auto amount_commitment               = generator * amount;
     auto expected_new_balance_commitment = generator * ( balance - amount ); // This is not valid but used to check the failure condition
 
-    EXPECT_FALSE( ValidateTransaction( balance, amount, balance_commitment, amount_commitment, expected_new_balance_commitment, generator, ranges,
-                                       base_seed, provided_totp ) );
+    EXPECT_FALSE( ValidateTransaction( balance, amount, balance, amount, balance_commitment, amount_commitment, expected_new_balance_commitment,
+                                       generator, ranges, base_seed, provided_totp ) );
 }
 
 TEST( TransactionVerifierCircuitTest, ValidateTransactionFailInvalidCommitments )
 {
     // Test parameters
-    typename pallas::scalar_field_type::value_type                         balance = 1000;
-    typename pallas::scalar_field_type::value_type                         amount  = 500;
+    uint64_t                                                               balance = 1000;
+    uint64_t                                                               amount  = 500;
     typename pallas::template g1_type<coordinates::affine>::value_type     generator( 1, 2 );
     typename pallas::scalar_field_type::value_type                         base_seed     = 12345; // Example seed for TOTP
     typename pallas::scalar_field_type::value_type                         provided_totp = 67890; // Example provided TOTP
@@ -63,15 +65,15 @@ TEST( TransactionVerifierCircuitTest, ValidateTransactionFailInvalidCommitments 
     auto amount_commitment               = generator * amount;
     auto expected_new_balance_commitment = generator * ( balance - amount + 1 ); // Use an incorrect new balance commitment
 
-    EXPECT_FALSE( ValidateTransaction( balance, amount, balance_commitment, amount_commitment, expected_new_balance_commitment, generator, ranges,
-                                       base_seed, provided_totp ) );
+    EXPECT_FALSE( ValidateTransaction( balance, amount, balance, amount, balance_commitment, amount_commitment, expected_new_balance_commitment,
+                                       generator, ranges, base_seed, provided_totp ) );
 }
 
 TEST( TransactionVerifierCircuitTest, ValidateTransactionPassZeroAmount )
 {
     // Test parameters
-    typename pallas::scalar_field_type::value_type                         balance = 1000;
-    typename pallas::scalar_field_type::value_type                         amount  = 0;
+    uint64_t                                                               balance = 1000;
+    uint64_t                                                               amount  = 0;
     typename pallas::template g1_type<coordinates::affine>::value_type     generator( 1, 2 );
     typename pallas::scalar_field_type::value_type                         base_seed     = 12345; // Example seed for TOTP
     typename pallas::scalar_field_type::value_type                         provided_totp = 67890; // Example provided TOTP
@@ -82,15 +84,15 @@ TEST( TransactionVerifierCircuitTest, ValidateTransactionPassZeroAmount )
     auto amount_commitment               = generator * amount;
     auto expected_new_balance_commitment = generator * balance;
 
-    EXPECT_TRUE( ValidateTransaction( balance, amount, balance_commitment, amount_commitment, expected_new_balance_commitment, generator, ranges,
-                                      base_seed, provided_totp ) );
+    EXPECT_TRUE( ValidateTransaction( balance, amount, balance, amount, balance_commitment, amount_commitment, expected_new_balance_commitment,
+                                      generator, ranges, base_seed, provided_totp ) );
 }
 
 TEST( TransactionVerifierCircuitTest, ValidateTransactionPassExactBalance )
 {
     // Test parameters
-    typename pallas::scalar_field_type::value_type                         balance = 1000;
-    typename pallas::scalar_field_type::value_type                         amount  = 1000;
+    uint64_t                                                               balance = 1000;
+    uint64_t                                                               amount  = 1000;
     typename pallas::template g1_type<coordinates::affine>::value_type     generator( 1, 2 );
     typename pallas::scalar_field_type::value_type                         base_seed     = 12345; // Example seed for TOTP
     typename pallas::scalar_field_type::value_type                         provided_totp = 67890; // Example provided TOTP
@@ -101,6 +103,6 @@ TEST( TransactionVerifierCircuitTest, ValidateTransactionPassExactBalance )
     auto amount_commitment               = generator * amount;
     auto expected_new_balance_commitment = generator * 0;
 
-    EXPECT_TRUE( ValidateTransaction( balance, amount, balance_commitment, amount_commitment, expected_new_balance_commitment, generator, ranges,
-                                      base_seed, provided_totp ) );
+    EXPECT_TRUE( ValidateTransaction( balance, amount, balance, amount, balance_commitment, amount_commitment, expected_new_balance_commitment,
+                                      generator, ranges, base_seed, provided_totp ) );
 }
